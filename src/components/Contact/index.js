bb678b4c84c0d2f5ab48a1f35337c7b0a1cf320a
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-04-18 23:38:05 -0500

    Validate the Form Data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
 const [formState, setFormState] = useState({ name: '', email: '', message: '' }); 
 const { name, email, message } = formState;
+const [errorMessage, setErrorMessage] = useState('');
 
     function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                    setErrorMessage('');
+                }
+            }
+        }
+        if (!errorMessage) {
         setFormState({...formState, [e.target.name]: e.target.value })
+        }
     }
 
     function handleSubmit(e) {
